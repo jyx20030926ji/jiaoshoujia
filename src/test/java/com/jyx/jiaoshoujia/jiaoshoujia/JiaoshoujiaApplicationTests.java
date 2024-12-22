a4b385cfd10d073ee48c9188fea9916c24b4f1a1
jyx20030926ji <2736805640@qq.com>
2024-12-22 14:59:41 +0800

    spring基本项目的脚手架

---
+++ src/test/java/com/jyx/jiaoshoujia/jiaoshoujia/JiaoshoujiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyx.jiaoshoujia.jiaoshoujia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiaoshoujiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
